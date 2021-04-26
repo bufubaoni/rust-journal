78737e327c7654dcc652197bb8f04bee792a227d
陈新 <xin.chen3@domob.cn>
2021-04-26 16:47:45 +0800

    添加 task 的序列化 和反序列化，添加三个虚方法

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,8 +1,12 @@
-use chrono::{DateTime, Utc};
+use chrono::{serde::ts_seconds, DateTime, Local, Utc}
+use serde::Deserialize;
+use serde::Serialize;
 
-#[derive(Debug)]
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Task {
     pub text: String,
+    #[serd(with = "ts_seconds")]
     pub created_at: DateTime<Utc>,
 }
 
@@ -12,3 +16,11 @@ impl Task {
         Task { text, created_at }
     }
 }
+
+
+use std::io::Result;
+use std::path::PathBuf;
+
+pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()>{...}
+pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()>{...}
+pub fn list_task(journal_path: PathBuf) -> Result<()>{...}
